558bad9750617fc86f5a126604c861438f47dfd2
orange <wisdom_xu666@sina.com>
2019-12-05 20:59:52 +0800

    feat: 去掉文件请求的withCredentials

--- src/utils/request.js
+++ src/utils/request.js
@@ -27,7 +27,7 @@ function camelize(obj) {
 // create an axios instance
 const service = axios.create({
   baseURL: process.env.VUE_APP_BASE_API, // url = base url + request url
-  withCredentials: true, // send cookies when cross-domain requests
+  // withCredentials: true, // send cookies when cross-domain requests
   timeout: 5000 // request timeout
 })
 
@@ -39,12 +39,16 @@ service.interceptors.request.use(
     if (uploadIndex > -1) {
       config.baseURL = process.env.VUE_APP_FILE_API || ''
     }
-    if (store.getters.token && uploadIndex === -1) {
-      // let each request carry token
-      // ['X-Token'] is a custom headers key
-      // please modify it according to the actual situation
-      config.headers['Authorization'] = `Bearer ${getToken()}`
+    if (uploadIndex === -1) {
+      config.withCredentials = true
+      if (store.getters.token) {
+        // let each request carry token
+        // ['X-Token'] is a custom headers key
+        // please modify it according to the actual situation
+        config.headers['Authorization'] = `Bearer ${getToken()}`
+      }
     }
+    console.log(config)
     return config
   },
   error => {
